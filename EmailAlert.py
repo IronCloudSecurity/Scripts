552a6433b28be019de179889216e834ab0090e5b
Sierra Maldonado <122700822+Magicwolfes@users.noreply.github.com>
2023-05-17 18:04:46 -0400

    Update EmailAlert.py

--- EmailAlert.py
+++ EmailAlert.py
@@ -1,47 +1,19 @@
+import json
 import boto3
 
-def lambda_handler(event, context):
-    # Extract instance ID from the event
-    instance_id = event['detail']['instance-id']
+def lambda_handler(event,context):
+    client = bot3.client('guardduty')
+    response = client.get_findings()
     
-    # Check if the event is related to a brute force attack
-    if event['detail']['eventName'] == 'ConsoleLogin' and event['detail']['errorMessage'] == 'Failed authentication':
-        # Create a DynamoDB client
-        dynamodb_client = boto3.client('dynamodb')
+    for findings in response['FindingIDs']:
+        findings_details = client.get_findings(findingsIDs=findings)
         
-        # Check if the instance has a counter in the DynamoDB table
-        response = dynamodb_client.get_item(
-            TableName='FailedAttemptsTable',
-            Key={'InstanceID': {'S': instance_id}}
-        )
-        
-        if 'Item' not in response:
-            # If the instance does not have a counter, initialize it to 1
-            dynamodb_client.put_item(
-                TableName='FailedAttemptsTable',
-                Item={'InstanceID': {'S': instance_id}, 'Attempts': {'N': '1'}}
-            )
-        else:
-            # If the instance has a counter, increment it by 1
-            attempts = int(response['Item']['Attempts']['N'])
-            attempts += 1
-            
-            if attempts >= 3:
-                # If the failed attempts exceed the threshold, send an email notification
-                # Create an SNS client
-                sns_client = boto3.client('sns')
-                
-                # Publish a message to the SNS topic
-                response = sns_client.publish(
-                    TopicArn='arn:aws:sns:us-east-2:952514564423:aws-cloudtrail-logs2:68b30b62-ead0-4e6d-aa8b-683322499727',
-                    Message=f"Brute force attack detected on EC2 instance {instance_id}. Failed attempts: {attempts}"
-                )
-                
-                # Print the response to check for any errors
-                print(response)
-            
-            # Update the counter in the DynamoDB table
-            dynamodb_client.put_item(
-                TableName='FailedAttemptsTable',
-                Item={'InstanceID': {'S': instance_id}, 'Attempts': {'N': str(attempts)}}
-            )
+        if finding_details['Severity'] > 4.0:
+            take_action(findings_details)
+def take_action(findings):
+    sns = boto3.client('sns')
+    response = sns.publish(
+        TopicARN='arn:aws:sns:us-east-2:952514564423:BruteForce:b4f525f1-4106-401f-bac5-8b13cd4fc604',
+        Message=json.dumps(finding),
+        Subject="GuardDuty Findings"
+    )
